11e21ba449f1889739bb305d721d43caf97a636b
Daniel Rovira Martínez <drovmar@upv.es>
2023-04-24 16:10:34 +0200

    Home

--- lib/views/products_list_view.dart
+++ lib/views/products_list_view.dart
@@ -13,9 +13,25 @@ class _ProductsPageState extends State<ProductsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(kToolbarHeight),
+            child: Container(
+              color: Colors.blue, // agregamos el color de fondo azul
+              child: SafeArea(
+                child: Center(
+                  child: Text(
+                    "Home",
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                          color: Colors
+                              .white, // establecemos el color de texto en blanco
+                        ),
+                  ),
+                ),
+              ),
+            )),
         body: ListView.builder(
-      itemCount: widget.productsJson.length,
-      itemBuilder: (context, i) => widget.productsJson[i],
-    ));
+          itemCount: widget.productsJson.length,
+          itemBuilder: (context, i) => widget.productsJson[i],
+        ));
   }
 }
